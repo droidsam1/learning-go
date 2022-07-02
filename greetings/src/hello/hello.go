231529f1c763276fec3c9f53e4bece7c99f84cf5
Samuel Taboas <droidsam@gmail.com>
2022-07-02 19:46:30 +0200

    add error greeting handling

--- greetings/src/hello/hello.go
+++ greetings/src/hello/hello.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greeting"
 )
 
 func main() {
-	message := greeting.Hello("Sam")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greeting.Hello("Sam")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
